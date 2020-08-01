78e40a4e661761dbba90cd44e1e832c179f0712a
Artem Zakirullin <artemzr@gmail.com>
2020-08-01 11:12:51 +0300

    add custom type finder

---
+++ src/Finder/ListOfTypeFinder.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace Zakirullin\TypedAccessor\Finder;
+
+use function is_int;
+
+final class ListOfTypeFinder
+{
+    public static function find($value, callable $typeChecker): ?array
+    {
+        $listOfMixed = ListOfMixedFinder::find($value);
+        if ($listOfMixed === null) {
+            return null;
+        }
+
+        /**
+         * @psalm-var list $listOfMixed
+         */
+
+        /**
+         * @psalm-suppress all
+         */
+        foreach ($listOfMixed as $val) {
+            if ($typeChecker($val)) {
+                return null;
+            }
+        }
+
+        /**
+         * @psalm-var list<int> $listOfMixed
+         */
+
+        return $listOfMixed;
+    }
+}
