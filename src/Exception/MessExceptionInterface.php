670466d5a1d4d241dee0ba7b333c1dfac0d5b772
Artem Zakirullin <artemzr@gmail.com>
2020-08-03 17:52:09 +0300

    add throwable to interface

--- src/Exception/MessExceptionInterface.php
+++ src/Exception/MessExceptionInterface.php
@@ -3,7 +3,9 @@ declare(strict_types=1);
 
 namespace Zakirullin\Mess\Exception;
 
-interface MessExceptionInterface
+use Throwable;
+
+interface MessExceptionInterface extends Throwable
 {
     /**
      * @psalm-return list<string|int>
